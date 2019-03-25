8e70c2c0976d3c285eb8b3eaa0a11dcd900b12cc
Formacion <Formacion@DESKTOP-761N3EA>
2019-03-25 17:31:06 +0100

    Primer Commit

--- jpa001Maven/src/main/java/es/avalon/jpa/negocio/Libro.java
+++ jpa001Maven/src/main/java/es/avalon/jpa/negocio/Libro.java
@@ -1,33 +1,20 @@
 package es.avalon.jpa.negocio;
 
 public class Libro {
-	
-	
+
 	private String titulo;
 	private String autor;
 	private int paginas;
 
-	public String getTitulo() {
-		return titulo;
+	public Libro() {
+		super();
 	}
-	public void setTitulo(String titulo) {
+
+	public Libro(String titulo) {
+		super();
 		this.titulo = titulo;
 	}
-	public String getAutor() {
-		return autor;
-	}
-	public void setAutor(String autor) {
-		this.autor = autor;
-	}
-	public int getPaginas() {
-		return paginas;
-	}
-	public void setPaginas(int paginas) {
-		this.paginas = paginas;
-	}
-	
-	
-	
+
 	public Libro(String titulo, String autor, int paginas) {
 		super();
 		this.titulo = titulo;
@@ -35,7 +22,53 @@ public class Libro {
 		this.paginas = paginas;
 	}
 
+	public String getTitulo() {
+		return titulo;
+	}
 
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
 
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public int getPaginas() {
+		return paginas;
+	}
+
+	public void setPaginas(int paginas) {
+		this.paginas = paginas;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((titulo == null) ? 0 : titulo.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Libro other = (Libro) obj;
+		if (titulo == null) {
+			if (other.titulo != null)
+				return false;
+		} else if (!titulo.equals(other.titulo))
+			return false;
+		return true;
+	}
 	
 }
