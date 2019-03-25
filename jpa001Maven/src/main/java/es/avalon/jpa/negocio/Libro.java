05f5bf6fbf986a2330c48ec3d4558810a4aa6016
cecilioalvarezcaules <cecilioalvarezcaules@192.168.193.1>
2019-03-25 17:24:05 +0100

    clase libro

---
+++ jpa001Maven/src/main/java/es/avalon/jpa/negocio/Libro.java
@@ -0,0 +1,41 @@
+package es.avalon.jpa.negocio;
+
+public class Libro {
+	
+	
+	private String titulo;
+	private String autor;
+	private int paginas;
+
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getAutor() {
+		return autor;
+	}
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+	public int getPaginas() {
+		return paginas;
+	}
+	public void setPaginas(int paginas) {
+		this.paginas = paginas;
+	}
+	
+	
+	
+	public Libro(String titulo, String autor, int paginas) {
+		super();
+		this.titulo = titulo;
+		this.autor = autor;
+		this.paginas = paginas;
+	}
+
+
+
+	
+}
