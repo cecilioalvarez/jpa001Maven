8147bb8dde384740427ebaef63b1e8e5c8ba644d
Formacion <Formacion@DESKTOP-630H6V9>
2019-03-28 18:54:53 +0100

    commit 28/3 jpaMaven

---
+++ jpa001Maven/src/main/java/es/avalon/jpa/negocio/Capitulo.java
@@ -0,0 +1,84 @@
+package es.avalon.jpa.negocio;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Capitulo {
+	
+	@Id
+	private String titulo;
+	private int paginas;
+	//defino una propiedad que es un objeto complejo utilizando composicion
+	@ManyToOne
+	@JoinColumn(name="Libro_Titulo")  //relacion entre Libro Capitulo
+	private Libro libro;
+	
+	
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public int getPaginas() {
+		return paginas;
+	}
+	public void setPaginas(int paginas) {
+		this.paginas = paginas;
+	}
+	public Capitulo(String titulo, int paginas) {
+		super();
+		this.titulo = titulo;
+		this.paginas = paginas;
+	}
+	
+	public Capitulo(String titulo) {
+		super();
+		this.titulo = titulo;
+	}
+	
+	public Capitulo() {
+		super();
+	}
+	
+	public Libro getLibro() {
+		return libro;
+	}
+	public void setLibro(Libro libro) {
+		this.libro = libro;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((titulo == null) ? 0 : titulo.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Capitulo other = (Capitulo) obj;
+		if (titulo == null) {
+			if (other.titulo != null)
+				return false;
+		} else if (!titulo.equals(other.titulo))
+			return false;
+		return true;
+	}
+	public Capitulo(String titulo, int paginas, Libro libro) {
+		super();
+		this.titulo = titulo;
+		this.paginas = paginas;
+		this.libro = libro;
+	}
+	
+	
+}
--- jpa001Maven/src/main/java/es/avalon/jpa/negocio/Libro.java
+++ jpa001Maven/src/main/java/es/avalon/jpa/negocio/Libro.java
@@ -1,16 +1,39 @@
 package es.avalon.jpa.negocio;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
-
 public class Libro {
 	
 	@Id
 	private String titulo;
 	private String autor;
 	private int paginas;
+	@OneToMany(mappedBy="libro")  //relacion entre Libro Capitulo
+	//se refiere a la propiedad libro de la clase Capitulo
+	private List<Capitulo> capitulos = new ArrayList<Capitulo>();
+
+	public void addCapitulo(Capitulo c) {
+		this.capitulos.add(c);
+	}
+	
+	public void removeCapitulo(Capitulo c) {
+		this.capitulos.remove(c);
+	}
+	
+	
+	public List<Capitulo> getCapitulos() {
+		return capitulos;
+	}
+
+	public void setCapitulos(List<Capitulo> capitulos) {
+		this.capitulos = capitulos;
+	}
 
 	public String getTitulo() {
 		return titulo;
@@ -43,6 +66,29 @@ public class Libro {
 		this.titulo = titulo;
 		this.autor = autor;
 		this.paginas = paginas;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((titulo == null) ? 0 : titulo.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Libro other = (Libro) obj;
+		if (titulo == null) {
+			if (other.titulo != null)
+				return false;
+		} else if (!titulo.equals(other.titulo))
+			return false;
+		return true;
 	}
 
 
